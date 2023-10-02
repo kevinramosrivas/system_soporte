418c10a9c6879b3064d2df33b1b803f0f998a478
Kevin Ramos <mc6.62x10.34@gmail.com>
2023-10-02 15:20:54 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+function onScanSuccess(decodedText, decodedResult) {
+    // handle the scanned code as you like, for example:
+    console.log(`Code matched = ${decodedText}`, decodedResult);
+    // actualizar el valor del input con el valor del código QR
+    document.getElementById('numero_documento').value = decodedText;
+  }
+  
+  function onScanFailure(error) {
+    // handle scan failure, usually better to ignore and keep scanning.
+    // for example:
+    //console.warn(`Code scan error = ${error}`);
+  }
+  
+  let html5QrcodeScanner = new Html5QrcodeScanner(
+    "reader",
+    { fps: 40, qrbox: {width: 250, height: 250} },
+    /* verbose= */ false);
+  html5QrcodeScanner.render(onScanSuccess, onScanFailure);
