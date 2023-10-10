8e619105bede9030f325e4380916b922a194df77
Kevin Ramos Rivas <mc6.62x10.34@gmail.com>
2023-10-09 21:23:47 -0500

    add conection to database

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import { saveRegister } from "./firebase.js";
 function onScanSuccess(decodedText, decodedResult) {
     // handle the scanned code as you like, for example:
     console.log(`Code matched = ${decodedText}`, decodedResult);
@@ -22,3 +23,56 @@ html5QrcodeScanner.render(onScanSuccess, onScanFailure);
 window.addEventListener('load', () => {
 
 });
+
+const registerForm = document.getElementById('form_register_student');
+
+registerForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(registerForm);
+    const data = {
+        numero_laboratorio: formData.get('num_laboratorio'),
+        tipo_documento: formData.get('tipo_documento'),
+        numero_documento: formData.get('numero_documento')
+    };
+    // validar los datos del formulario
+    const isValid = validateForm(data.numero_laboratorio, data.tipo_documento, data.numero_documento);
+    if(isValid){
+        saveRegister(data.numero_laboratorio, data.tipo_documento, data.numero_documento);
+    }
+
+});
+
+const validateForm = (num_laboratorio, tipo_documento, numero_documento) => {
+    if(num_laboratorio === '' || tipo_documento === '' || numero_documento === '') {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Todos los campos son obligatorios!'
+        })
+        return false;
+    }
+    else {
+        // validar que el número de laboratorio sea un número mediante expresiones regulares
+        const regex_num_laboratorio = /^[0-9]+$/;
+        if(!regex_num_laboratorio.test(num_laboratorio)) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'El número de laboratorio debe ser un número!'
+            })
+            return false;
+        }
+        // validar que el número de documento sea un número mediante expresiones regulares
+        const regex_num_documento = /^[0-9]+$/;
+        if(!regex_num_documento.test(numero_documento)) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'El número de documento debe ser un número!'
+            })
+            return false;
+        }
+        return true;
+
+    }
+}
--- firebase.js
+++ firebase.js
@@ -2,6 +2,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js";
+import { getFirestore, collection, addDoc, serverTimestamp, getDocs, query, where
+} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,5 +22,52 @@ measurementId: "G-WLW8HPFEBW"
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export const saveRegister = async(num_laboratorio, tipo_documento, numero_documento) => {
+    //verificar si existe un registro previo el mismo día
+    // si es asi regitrar como salida y si no como entrada
+    const q = query(collection(db, "registers"));
+    const q1 = query(q, where("tipo_documento", "==", tipo_documento), where("numero_documento", "==", numero_documento));
+    const q2 = query(q1, where("tipo_registro", "==", "entrada"));
+    const querySnapshot = await getDocs(q2);
+    if (querySnapshot.empty) {
+        console.log("No hay registros de entrada");
+        addRegister(num_laboratorio, tipo_documento, numero_documento, "entrada");
+    } else {
+        console.log("Hay registros de entrada");
+        addRegister(num_laboratorio, tipo_documento, numero_documento, "salida");
+    }
+}
+
+
+const addRegister = (num_laboratorio, tipo_documento, numero_documento, tipo_registro) => {
+        // añadir firestore timestamp
+        addDoc(collection(db, "registers"), {
+            num_laboratorio: num_laboratorio,
+            tipo_documento: tipo_documento,
+            numero_documento: numero_documento,
+            tipo_registro: tipo_registro,
+            data_created: serverTimestamp()
+        })
+        .then((docRef) => {
+            console.log("Document written with ID: ", docRef.id);
+            Swal.fire(
+                'Registrado!',
+                `El registro de {tipo_registro} fue exitoso!`,
+                'success'
+            )
+        })
+        .catch((error) => {
+            console.error("Error adding document: ", error);
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Algo salió mal!'
+            })
+        });
+}
+
+
 
 
